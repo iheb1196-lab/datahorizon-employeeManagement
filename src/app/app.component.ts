c6499b73ed81e332b2be9d95544a776644a93be3
iheb <iheb.j@xgol.pro>
2025-01-14 23:10:29 +0000

    spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+
 import { EmployeesComponent } from './components/employees/employees.component';
-//test
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent, EmployeesComponent],
+  imports: [CommonModule, HeaderComponent, EmployeesComponent],
   template: `
     <app-header></app-header>
     <main class="container mt-4">
       <app-employees></app-employees>
     </main>
-    <app-footer></app-footer>
+   
   `,
   styleUrls: ['./app.component.css'],
 })
