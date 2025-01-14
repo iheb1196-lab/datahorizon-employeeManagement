24c5788bc1e9edeeb5a5638b51915f908c88dba1
iheb <iheb.j@xgol.pro>
2025-01-14 21:51:17 +0000

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { EmployeesComponent } from './components/employees/employees.component';
-
+//test
 @Component({
   selector: 'app-root',
   standalone: true,
